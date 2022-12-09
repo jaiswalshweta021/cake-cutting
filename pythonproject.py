247a82a7275a91d8cfbd0349b11583f0a0f1e6a0
jaiswalshweta021 <120053764+jaiswalshweta021@users.noreply.github.com>
2022-12-09 14:52:01 +0530

    Update pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,13 +1,13 @@
 cakeangle=eval(input("Enter the Angle of the Cake: "))
 N=eval(input("Enter N: "))
 if(cakeangle%N==0):
-    print("YES the cake will cut in equal pieces of size %d"%N)
+    print("YES the cake will CUT in equal pieces of size %d"%N)
 else:
-    print("NO the cake will not cut in equal pieces of size %d"%N)
+    print("NO the cake will not CUT in equal pieces of size %d"%N)
 if(N>cakeangle): #Only when N is greater tha cake angle cake can't be cut into pieces
     print("NO the cake will not cut in any piece of size %d"%N)
 else:
-    print("YES the cake will cut in any piece of size %d"%N)
+    print("YES the cake will CUT in any piece of size %d"%N)
 n=1 # start subratcrting the cake
 for i in range(N):
     cakeangle-=n
